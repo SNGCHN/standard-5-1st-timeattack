e2a16a3b7a763abff8836964ddf23618b036c3b4
SNGCHN LEE <sngchn0312@naver.com>
2024-05-20 15:54:00 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 export default function App() {
+  // const [cal, setCal] = useState("");
+
+  const plus = (number) => {};
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input id='number' /> 만큼을 <button id='plus' onClick={}>더할게요</button> <button id='minus'>뺄게요</button>
+        <button id='reset'>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p id='result'>0</p>
       </div>
     </div>
   );
